098eefc01662863617fb6dafb7fbdd7ad2face69
Richardson <ricardoelias556@gmail.com>
2024-06-13 17:56:39 -0600

    se agregan las rutas

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express=require('express')
 const app = express()
 const port=4000
+const comidasRoutes=require('./routes/comidaRoutes.js')
+const morgan = require('morgan');
 
-app.get('/',(req,res)=>{
-    res.json({mensaje:'Aplicacion Funcionando Correctamente'})
-})
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+
+app.use(morgan('dev'));
+
+app.use("/API",comidasRoutes)
+
+app.use((req,res)=>{
+    res.status(404).json({message: "No se encontro la ruta solicitada"})
+});
 
 app.listen(port,()=>{
     console.log(`Aplicacion corriendo en el puerto ${port}`)
